aa6c6d7b87a00947f4c802fcdd2eccfbdc038f2e
Thyeres Nahum <thyeres.nahum@gmail.com>
2017-07-06 16:57:40 -0300

    Acento corrigido

--- src/main/java/controller/ServletFuncionario.java
+++ src/main/java/controller/ServletFuncionario.java
@@ -34,7 +34,7 @@ public class ServletFuncionario extends HttpServlet {
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-
+		request.setCharacterEncoding("UTF8");
 		acao = false;
 
 		try {
